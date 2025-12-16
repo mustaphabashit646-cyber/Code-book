0187c152aaa9052595b8a3c94a903ade9e38d5d8
Bashit Mustapha <mustaphabashit646@gmail.com>
2025-12-16 10:45:03 +0100

    live api configured

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://code-book-backend-igun.onrender.com/",
         changeOrigin: true,
         secure: false
       }
